5e297a0c7ed1565d440e0d1e811f1158f9b4b3b1
Your Name <you@example.com>
2021-07-28 00:12:53 +0530

    useFetch finalized

--- components/Create/AddByGroup/SaveCreatedGroup.tsx
+++ components/Create/AddByGroup/SaveCreatedGroup.tsx
@@ -3,10 +3,12 @@ import groupsApi from "@apiClient/groupsApi";
 import Group from "./Group";
 import { deferrer, useFetch } from "@hooks/useFetch";
 import Button from "@components/Controls/Button";
+import { useRouter } from "next/router";
 interface SaveCreatedGroupProps {
   loopers: EntityLooper[];
 }
 const SaveCreatedGroup = ({ loopers }: SaveCreatedGroupProps) => {
+  const router = useRouter();
   const {
     data,
     loading,
@@ -16,7 +18,17 @@ const SaveCreatedGroup = ({ loopers }: SaveCreatedGroupProps) => {
     deferrer(groupsApi.create, {
       members: loopers,
     }),
-    { deferred: true }
+    {
+      deferred: true,
+      numRetriesOnError: 3,
+      retryMethodOnError: () => {
+        console.log("retry");
+      },
+      methodOnError: () => {
+        console.log("redirect to login page");
+        // router.push("/login");
+      },
+    }
   );
   if (!requestSent) {
     return (
--- hooks/useFetch.ts
+++ hooks/useFetch.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 export const deferrer = (func: Function, ...prevargs: any) => {
   return (...args: any) => {
     return func(...prevargs, ...args);
@@ -6,17 +6,40 @@ export const deferrer = (func: Function, ...prevargs: any) => {
 };
 export const useFetch = <T>(
   fetcher: Function,
-  { deferred = false }: { deferred?: boolean } = {}
+  {
+    deferred = false,
+    numRetriesOnError = 0,
+    retryMethodOnError = () => {},
+    methodOnError = () => {},
+  }: {
+    deferred?: boolean;
+    numRetriesOnError?: number;
+    retryMethodOnError?: Function;
+    methodOnError?: Function;
+  } = {}
 ) => {
   const [data, setData] = useState<T>();
   const [requestSent, setRequestSent] = useState(!deferred);
   const [loading, setLoading] = useState<boolean>(!deferred);
+  const remainingTriesOnError = useRef(numRetriesOnError);
+
   const sendRequest = async (...args: any) => {
     setRequestSent(true);
     setLoading(true);
     const res = await fetcher(...args);
-    setData(res);
-    setLoading(false);
+    if (res) {
+      setData(res);
+      setLoading(false);
+    } else {
+      if (remainingTriesOnError.current > 0) {
+        remainingTriesOnError.current--;
+        await retryMethodOnError();
+        sendRequest();
+      } else {
+        setLoading(false);
+        await methodOnError();
+      }
+    }
   };
   useEffect(() => {
     if (!deferred) {
