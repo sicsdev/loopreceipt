8debfd0b295b4002544546a843e5711c7a0c8718
Yash <sicsdev21@gmail.com>
2021-08-04 05:47:01 +0530

    done

--- practice.ts
+++ practice.ts
@@ -1,18 +1,1 @@
-export const splitOnUpperCase = (str: string) => {
-  // str = "MyNameIsKhan"
-  // returns "My Name Is Khan"
-  let ans = "";
-  let cur = "";
-  for (let s of str.split("")) {
-    if (/[A-Z]/.test(s)) {
-      // encountered uppercase character
-      ans += cur + " ";
-      cur = s;
-    } else {
-      cur += s;
-    }
-  }
-  ans += cur;
-  return ans;
-};
-console.log(splitOnUpperCase("MyNameIsKhan"));
+export const name = "rahul";
