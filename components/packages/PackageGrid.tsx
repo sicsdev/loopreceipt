766e22e56db668ecf3548498178f474883d52e31
Yash <sicsdev21@gmail.com>
2021-09-28 17:34:14 +0530

    Packages & Recipient Pages

---
+++ components/packages/PackageGrid.tsx
@@ -0,0 +1,268 @@
+import UPadWrapper from "@components/Shared/UPadWrapper";
+import MyLoader from "@components/Shared/MyLoader";
+import { useDateTypeFilterAndPagination } from "@components/Dashboard/Tabs/useDateTypeFilterAndPagination";
+import { makeStyles } from "@material-ui/core";
+
+import React, { useEffect, useMemo, useState } from "react";
+import { DateRange, LoopSource, LoopType } from "@interfaces/LoopTypes";
+import { useAppDispatch, useAppSelector } from "@store/hooks";
+import FilterDropdowns from "@components/Dashboard/FilterDropdowns";
+import Pagination from "@components/Dashboard/Pagination";
+import { useWindowDimensions } from "@hooks/useWindowDimensions";
+import Win from "@helpers/Win";
+import { useFetchReturnType } from "@hooks/useFetch";
+import MessageCard from "@components/Shared/MessageCard";
+import { useWaiter } from "@hooks/useWaiter";
+import { DataGrid, GridRowsProp, GridColDef } from '@mui/x-data-grid';
+import moment from "moment";
+import PackagesIcon from "@components/Shared/PackagesIcon";
+import { randomMemoizedColor } from "@helpers/utils";
+
+export const itemsPerPageOptions = [5, 10, 15];
+export interface StdData {
+  items: any[];
+  total: number;
+}
+interface TabsBaseProps {
+  getter: useFetchReturnType<StdData>;
+}
+
+const PackageGrid = ({ getter }: TabsBaseProps) => {
+  const styles = useStyles();
+  const [loopSource, setLoopSource] = useState<LoopSource>("all");
+  const { windowDimensions } = useWindowDimensions();
+  const { wait } = useWaiter(0);
+  const win = new Win(windowDimensions);
+  const [dateRange, setDateRange] = useState<DateRange>({
+    start: null,
+    end: null,
+  });
+  const [itemsPerPage, setItemsPerPage] = useState(itemsPerPageOptions[1]);
+  const [page, setPage] = useState(1);
+  const [noItems, setNoItems] = useState(true);
+  const [packagesToShow, setPackagesToShow] = useState([]);
+  
+  const columns: GridColDef[] = [
+    { field: 'receiver', headerName: "Receiver's info", width: 260 , disableColumnMenu:true},
+    { field: 'date', headerName: 'Date Created', width: 260, disableColumnMenu:true},
+    { field: 'contact', headerName: 'Contact info', width: 260, disableColumnMenu:true},
+    { 
+      field: 'looper',
+      headerName: 'Loopers',
+      width: 260, 
+      disableColumnMenu:true, 
+      renderCell(cellValues) {
+        if(cellValues?.row?.looper) {
+          const loopersName = cellValues?.row?.looper
+          return (
+            <PackagesIcon
+                firstAlphabets={loopersName.map((item: any) =>
+                  item.name[0].toUpperCase()
+                )}
+                colorStrings={loopersName.map((itm: any) =>
+                  randomMemoizedColor(itm.name + itm.email)
+                )}
+              />
+          )
+        }
+        return (
+          <b>{cellValues.row.looper}</b>
+        )
+      }
+    }
+  ];
+
+  const user = useAppSelector((state) => state.user.user);
+  
+  useEffect(() => {
+    if (noItems && getter.data?.total) {
+      setNoItems(false);
+    }
+  }, [getter.data]);
+
+  useDateTypeFilterAndPagination({
+    getter,
+    page,
+    dateRange,
+    loopSource,
+    setPage,
+  });
+
+  useEffect(() => {
+    const dataItems = getter.data ? getter.data.items : [];
+    if (dataItems.length > 0) {
+        const listOfItems = [] as any;
+        dataItems.map((items, i) => {
+            const recipientInfo = items.recipient.name + ", " + items.recipient.company
+            const dateCreated = moment(items.updatedAt).format("MM/DD/YYYY");
+            const contactInfo = items.recipient.email
+            const loopersList = [] as any;
+            items.loopers.map((looper: any) => {
+                loopersList.push(looper)
+            })
+            listOfItems.push({
+              id: i+1,
+              receiver: recipientInfo,
+              date: dateCreated,
+              contact: contactInfo,
+              looper: loopersList
+            })
+        })
+        setPackagesToShow(listOfItems)
+    }
+    else {
+      setPackagesToShow([])
+    }
+  }, [getter.data, user]);
+
+  return (
+    <div className={styles.right}>
+      <h2 className={styles.packageMainHead}>My Loops</h2>
+      <div className={styles.packageBox}>
+      <div className={styles.packagesHead}>
+        <div className={styles.pckgHeadOne}>
+          <h3 className={styles.pckgHeadTitle}>Reports</h3>
+          <button type='button' className={styles.pckgHeadAction}>+ Create Loopreceipt</button>
+        </div>
+        <div className={styles.pckgHeadTwo}>
+        <div
+          className="dropdowns"
+          style={{ display: noItems ? "none" : "block" }}
+        >
+          <FilterDropdowns
+            loopSource={loopSource}
+            setLoopSource={setLoopSource}
+            dateRange={dateRange}
+            setDateRange={setDateRange}
+          />
+        </div>
+        </div>
+      </div>
+      {wait || getter.loading ? (
+        <div style={{ paddingTop: "3rem" }}>
+          <MyLoader loaded={!getter.loading} />
+        </div>
+      ) : (getter.data?.items  && packagesToShow !== null) ? (
+        <>
+          <UPadWrapper>
+            <>
+                <div style={{ height: 520, width: '100%' }}>
+                <DataGrid rows={packagesToShow} columns={columns} checkboxSelection={true} hideFooter={true}/>
+                </div>
+
+                <div className={styles.packagePagination}>
+                  <Pagination
+                    totalItems={getter.data.total}
+                    itemsPerPageOptions={itemsPerPageOptions}
+                    itemsPerPage={itemsPerPage}
+                    setItemsPerPage={setItemsPerPage}
+                    page={page}
+                    setPage={setPage}
+                  />
+                </div>
+            </>
+          </UPadWrapper>
+        </>
+      ) : (
+        <div
+          style={{
+            padding: 20,
+          }}
+        >
+          <MessageCard type="warning">Some error occured</MessageCard>
+        </div>
+      )}
+      </div>
+    </div>
+  );
+};
+export default PackageGrid;
+const useStyles = makeStyles((theme) => ({
+  right: {
+    marginLeft: 250,
+    padding: "3rem 3rem",
+    // border: "2px solid blue",
+    [theme.breakpoints.down("sm")]: {
+      marginLeft: 0,
+      padding: "0",
+    },
+    "& .dropdowns": {
+      padding: "0 4%",
+      paddingTop: "0rem",
+      [theme.breakpoints.down("sm")]: {
+        paddingTop: "2rem",
+      },
+    },
+    "& .top": {
+      display: "flex",
+      justifyContent: "space-between",
+      padding: "1.5rem 4%",
+      "& .head": {
+        fontSize: "1.3rem",
+        fontWeight: "500",
+      },
+    },
+    "& .noLoopsMessage": {
+      textAlign: "center",
+      fontSize: 20,
+      marginTop: "4rem",
+    },
+    packagesGrid: {
+      marginBottom: "20px"
+    }
+  },
+  rest: {
+    "& .cards": {
+      // border: "1px solid red",
+      padding: "3rem 0",
+      display: "grid",
+      gridTemplateColumns: "repeat(3, 1fr)",
+      gap: 40,
+      [theme.breakpoints.down("md")]: {
+        gridTemplateColumns: "repeat(2, 1fr)",
+      },
+      [theme.breakpoints.down("xs")]: {
+        gridTemplateColumns: "repeat(1, 1fr)",
+      },
+    },
+  },
+  packageBox: {
+    border: "1px solid #DFDFDF",
+  },
+  packagesHead: {
+    display: "block",
+    overflow: "auto",
+    padding: "20px 0px 0px 20px"
+  },
+  pckgHeadOne: {
+    display: "inline-block",
+    width: "50%",
+  },
+  pckgHeadTwo: {
+    display: "inline-block",
+    width: "50%",
+    float: "right"
+  },
+  pckgHeadTitle: {
+    display: "inline-block",
+    fontSize: "20px",
+    fontWeight: 600    
+  },
+  pckgHeadAction: {
+    border: "1px solid #797979",
+    color: "#797979",
+    padding: "5px",
+    fontSize: "12px",
+    borderRadius: "2px",
+    cursor: "pointer",
+    marginLeft: "20px"
+  },
+  packagePagination: {
+    marginTop: "20px"
+  },
+  packageMainHead: {
+    fontSize: "22px",
+    fontWeight: 500,
+    marginBottom: "35px"
+  }
+}));
---
+++ pages/recipients.tsx
@@ -0,0 +1,80 @@
+import { makeStyles } from "@material-ui/core";
+import Sidebar from "@components/Navbar/DesktopSidebar";
+import Layout from "@components/Global/Layout";
+import Win from "@helpers/Win";
+import { useWindowDimensions } from "@hooks/useWindowDimensions";
+import AuthGuard from "@components/Global/AuthGuard";
+import { useFetch } from "@hooks/useFetch";
+import loopsApi from "@apiClient/loopsApi";
+import RecipientGrid from "@components/recipients/RecipientGrid";
+
+interface StdData {
+  items: any[];
+  total: number;
+}
+
+interface DashboardProps {
+  path: string;
+}
+
+const Packages = ({ path }: DashboardProps) => {
+  const styles = useStyles();
+  const getter = useFetch<StdData>(loopsApi.getAll, { deferred: true });
+  const { windowDimensions } = useWindowDimensions();
+  const win = new Win(windowDimensions);
+  return (
+    <AuthGuard>
+      <Layout>
+        <div className={styles.dashboard}>
+          {win.up("md") && <Sidebar path={path} />}
+          <div className={styles.right}>
+            <RecipientGrid getter={getter} />
+          </div>
+        </div>
+      </Layout>
+    </AuthGuard>
+  );
+};
+export default Packages;
+
+const useStyles = makeStyles((theme) => ({
+  dashboard: {},
+  right: {
+    backgroundColor: "#fbfbfb",
+    height: 400, 
+    width: '100%',
+    // border: "2px solid blue",
+    [theme.breakpoints.down("sm")]: {
+      marginLeft: 0,
+      padding: "0",
+    }
+  },
+  dataGrid: {
+    height: 400, 
+    width: '100%',
+    padding: "3rem 3rem",
+  },
+  iconGettingStarted: {
+    position: "fixed",
+    bottom: 40,
+    right: 50,
+    [theme.breakpoints.down("xs")]: {
+      right: 20,
+    },
+    userSelect: "none",
+    cursor: "pointer",
+    "& .icon": {
+      width: 61,
+      height: 61,
+
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      borderRadius: "50%",
+      backgroundColor: "#363F4D",
+    },
+    "& .MuiButtonBase-root": {
+      backgroundColor: "#363F4D",
+    },
+  },
+}));
